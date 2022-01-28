e9310b61ce32d350a36600fa8426a877a10a80bc
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-28 13:28:11 +0700

    success made input file

---
+++ pages/movies/[slug].js
@@ -0,0 +1,39 @@
+import Head from "next/head"
+import styles from "../../styles/Home.module.css"
+
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient();
+
+export default function Movie({ movie }) {
+    return(
+        <div className="{styles.container}">
+            <Head>
+                <title>{ movie.title }</title>
+                <link rel="stylesheet" href="/favicon.ico" />
+            </Head>
+
+            <main className="{styles.main}">
+                <h2>{ movie.title }</h2>
+                <p>{ movie.description }</p>
+            </main>
+        </div>
+    );
+}
+
+/* async function to get slug view */
+export async function getServerSideProps(context) {
+
+    const {slug} = context.query;
+
+    const movie = await prisma.movie.findFirst({
+        where: {
+            slug: slug
+        }
+    })
+
+    return {
+        props: {
+            movie
+        }
+    }
+}
